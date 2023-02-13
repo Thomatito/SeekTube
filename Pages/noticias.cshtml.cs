7cdac5606daef96ef12d7eb162b2bc21298e5d96
thomas.wolf1 <thomas.wolf1@JLE206D98>
2023-02-13 16:27:26 -0300

    Adicionar arquivos de projeto.

---
+++ Pages/noticias.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeekTube.Pages
+{
+    public class noticiasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
